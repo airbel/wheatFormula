4621a4eea2354c19707d85ad0f519d2db8aeb4b4
Aiebel.ooo <geamdjum@gmail.com>
2022-03-22 07:05:09 +0800

    V

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 
-var roomtempture = document.getElementById("roomtempture").value;
+var roomtempture = document.getElementById("roomtempture").value;   //室溫
+var waternum = document.getElementById("waternum").value;           //水溫
+var flotrTempture = document.getElementById("flotrTempture").value; //粉溫
+var flotrFriction = document.getElementById("flotrFriction").value; //摩擦係數
+
 var getresult = document.getElementById("getresult");
 var used = document.getElementById("used");
 
-function selectVa() {
 
+function test (){
+
+
+}
+
+
+var restart = 0;
+// function selectVa() {
     getresult.addEventListener("click", function () {
-        location.reload();
-        used.innerHTML = roomtempture;
+        var timeoutID = setTimeout(myAlert, 500);
+
+        function myAlert() {
+            //window.location.reload();
+            alert('已刷新！');
+        }
+        timeoutID;
+        var tempdata = [4];
+        tempdata[0] = roomtempture;
+        tempdata[1] = waternum
+        tempdata[2] = flotrTempture
+        tempdata[3] = flotrFriction
+
+        tempdata.forEach(function(v,i){
+            used.innerHTML +=tempdata[i]+"<br>";
+        })
+        if (tempdata[0] != roomtempture &&　tempdata[1] != waternum){
+            window.location.reload();
+        }
+        // used.innerHTML = tempdata[0]+"<br>";
+        //getresult();
+        // if (typeof(usedf) == "number"){
+        //     location.reload();
+        // }
+        // usedf.innerHTML = typeof(roomtempture);
+        used.innerHTML += roomtempture +'/'+waternum+'/'+flotrTempture+'/'+flotrFriction+"<br>";
+        // window.location.reload();
+        clearTimeout(timeoutID);
+        return false;
     })
+// }
+
+function getresult (){
+    if(used.value == "") {
+
+    }else {
+        location.reload()
+    }
 }
