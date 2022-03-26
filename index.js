c3b3d0a0164111e08be85da8fb0d5560127bd3cf
Aiebel.ooo <geamdjum@gmail.com>
2022-03-26 08:07:08 +0800

    V2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var flotrTempture = document.getElementById("flotrTempture"); //粉溫
 var flotrFriction = document.getElementById("flotrFriction"); //摩擦係數
 var wannatempture = document.getElementById("wannatempture"); //理想溫度
 var flotrfinishTemp = document.getElementById("FlotrfinishTemp"); //麵團完成溫度
+var waterCalculation = document.getElementById("waterCalculation"); //配方水量
 
 var getresult = document.getElementById("getresult");
 var used = document.getElementById("used");
@@ -12,16 +13,19 @@ var used = document.getElementById("used");
 var restart = 0;
 
     getresult.addEventListener("click", function () {
-        var tempdata = [6];
+        var tempdata = [7];
         tempdata[0] = roomtempture.value;
         tempdata[1] = waternum.value;
         tempdata[2] = flotrTempture.value;
         tempdata[3] = flotrFriction.value;
         tempdata[4] = wannatempture.value;
         tempdata[5] = flotrfinishTemp.value;
+        tempdata[6] = waterCalculation.value;
+
 
         used.innerHTML = "室溫"+tempdata[0] +';<br> 水溫'+ waternum.value +';<br> 麵粉溫'+flotrTempture.value+';<br> 模擦係數'+flotrFriction.value+'<br> 理想溫度'+flotrFriction.value;
         used.innerHTML += '<br>******<br>摩擦係數:'+((Number(tempdata[5])*3)-(Number(tempdata[0])+Number(tempdata[2])+Number(tempdata[1])));
-        used.innerHTML += '<br>******<br>預計使用冰塊:'+((Number(tempdata[4])*3)-(Number(tempdata[0])+Number(tempdata[2])+Number(tempdata[3])));
+        used.innerHTML += '<br>******<br>適用水溫:'+((Number(tempdata[4])*3)-(Number(tempdata[0])+Number(tempdata[2])+Number(tempdata[3])));
+        used.innerHTML += '<br>******<br>冰量:'+((Number(tempdata[6])*3)*(Number(tempdata[0])+Number(tempdata[2])+Number(tempdata[3])));
         return false;
     })
